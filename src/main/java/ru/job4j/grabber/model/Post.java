c0a6bad35567b981b0ee10dafcf9c9e96cc65ecd
emelkin-pro <emelkin-pro@yandex.ru>
2025-11-20 20:54:50 +0300

    Модель данных - Post

---
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -0,0 +1,64 @@
+package ru.job4j.grabber.model;
+
+import java.util.Objects;
+
+public class Post {
+    long id;
+    String title = new String();
+    String link = new String();
+    String description = new String();
+    long time;
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public long getTime() {
+        return time;
+    }
+
+    public void setTime(long time) {
+        this.time = time;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Post post = (Post) o;
+        return id == post.id && time == post.time;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, time);
+    }
+}
