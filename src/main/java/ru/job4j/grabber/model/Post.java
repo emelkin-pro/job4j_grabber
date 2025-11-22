29de57d65cde04cefda4a3e3742da878eb1357c0
emelkin-pro <emelkin-pro@yandex.ru>
2025-11-22 15:52:41 +0300

    Модель данных - Post

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -54,11 +54,11 @@ public class Post {
         if (this == o) return true;
         if (o == null || getClass() != o.getClass()) return false;
         Post post = (Post) o;
-        return time == post.time && Objects.equals(title, post.title) && Objects.equals(link, post.link) && Objects.equals(description, post.description);
+        return Objects.equals(link, post.link);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(title, link, description, time);
+        return Objects.hash(link);
     }
 }
