73be919cee2ebc5da4c9832f960eff6e0b4a2706
emelkin-pro <emelkin-pro@yandex.ru>
2025-12-10 21:12:05 +0300

    2. Парсинг HTML страницы.

--- src/main/java/ru/job4j/grabber/model/Post.java
+++ src/main/java/ru/job4j/grabber/model/Post.java
@@ -65,4 +65,17 @@ public class Post {
     public int hashCode() {
         return Objects.hash(link);
     }
+
+    @Override
+    public String toString() {
+        return "Post{"
+                + "id="
+                + id + ", title='" + title + '\''
+                + ", link='" + link + '\''
+                + ", description='" + description + '\''
+                + ", time=" + time
+                + '}';
+    }
+
+
 }
