554fc6f9dee527786e7332753de069c8d8df9236
emelkin-pro <emelkin-pro@yandex.ru>
2025-12-15 21:33:59 +0300

    2.1.1. Парсинг

--- src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
+++ src/main/java/ru/job4j/grabber/service/HabrCareerParse.java
@@ -2,7 +2,9 @@ package ru.job4j.grabber.service;
 
 import org.apache.log4j.Logger;
 import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
 import ru.job4j.grabber.model.Post;
+
 import java.io.IOException;
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
@@ -13,37 +15,47 @@ public class HabrCareerParse implements Parse {
     private static final String SOURCE_LINK = "https://career.habr.com";
     private static final String PREFIX = "/vacancies?page=";
     private static final String SUFFIX = "&q=Java%20developer&type=all";
+    private static final int TOTAL_PAGES = 5;
 
     @Override
     public List<Post> fetch() {
         var result = new ArrayList<Post>();
         try {
-            int pageNumber = 1;
-            String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
-            var connection = Jsoup.connect(fullLink);
-            var document = connection.get();
-            var rows = document.select(".vacancy-card__inner");
-            rows.forEach(row -> {
-                var titleElement = row.select(".vacancy-card__title").first();
-                var linkElement = titleElement.child(0);
-                var time = OffsetDateTime.parse(row
-                        .select(".vacancy-card__date time")
-                        .attr("datetime"));
-                long timeElement = time.toInstant().toEpochMilli();
-                String vacancyName = titleElement.text();
-                String link = String.format("%s%s", SOURCE_LINK,
-                        linkElement.attr("href"));
-                var post = new Post();
-                post.setTitle(vacancyName);
-                post.setLink(link);
-                post.setTime(timeElement);
-                result.add(post);
-            });
+            for (int i = 1; i <= TOTAL_PAGES; i++) {
+                int pageNumber = i;
+                String fullLink = "%s%s%d%s".formatted(SOURCE_LINK, PREFIX, pageNumber, SUFFIX);
+                var connection = Jsoup.connect(fullLink);
+                var document = connection.get();
+                htmlParser(document, result);
+            }
         } catch (IOException e) {
             LOG.error("When load page", e);
         }
         return result;
     }
+
+    private void htmlParser(Document document, List<Post> result) {
+        var rows = document.select(".vacancy-card__inner");
+        rows.forEach(row -> {
+            var titleElement = row.select(".vacancy-card__title").first();
+            var linkElement = titleElement.child(0);
+            var time = OffsetDateTime.parse(row
+                    .select(".vacancy-card__date time")
+                    .attr("datetime"));
+            long timeElement = time.toInstant().toEpochMilli();
+            String vacancyName = titleElement.text();
+            String link = String.format("%s%s", SOURCE_LINK,
+                    linkElement.attr("href"));
+            var post = new Post();
+            post.setTitle(vacancyName);
+            post.setLink(link);
+            post.setTime(timeElement);
+            result.add(post);
+        });
+    }
+
+    ;
+
 
     public static void main(String[] args) {
         Parse parse = new HabrCareerParse();
